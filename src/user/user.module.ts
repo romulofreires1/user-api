62ef067ab4108639ca0c76bab699485d05fd1cb4
Romulo Freires <romulofreires1@gmail.com>
2024-09-02 15:02:14 -0300

    build: prometheus setup

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,42 @@ import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 import { EncryptionService } from './encryption.service';
+import {
+  makeCounterProvider,
+  PrometheusModule,
+} from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService, UserRepository, EncryptionService],
+  imports: [TypeOrmModule.forFeature([User]), PrometheusModule.register()],
+  providers: [
+    UserService,
+    UserRepository,
+    EncryptionService,
+    makeCounterProvider({
+      name: 'user_created_total',
+      help: 'Total number of users created',
+    }),
+    makeCounterProvider({
+      name: 'user_find_all_total',
+      help: 'Total number of findAll calls',
+    }),
+    makeCounterProvider({
+      name: 'user_find_one_total',
+      help: 'Total number of findOne calls',
+    }),
+    makeCounterProvider({
+      name: 'user_update_total',
+      help: 'Total number of update calls',
+    }),
+    makeCounterProvider({
+      name: 'user_remove_total',
+      help: 'Total number of remove calls',
+    }),
+    makeCounterProvider({
+      name: 'user_find_by_email_total',
+      help: 'Total number of findByEmail calls',
+    }),
+  ],
   controllers: [UserController],
   exports: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,17 +6,32 @@ import { User } from './user.entity';
 import { UserResponseDto } from './dto/user-response.dto';
 import { EncryptionService } from './encryption.service';
 import { CustomLoggerService } from 'src/common/logger/custom-logger.service';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
+import { Counter } from 'prom-client';
 
 @Injectable()
 export class UserService {
   private readonly logger = CustomLoggerService.getLogger('RequestLogger');
 
   constructor(
+    @InjectMetric('user_created_total')
+    private readonly userCreatedCounter: Counter,
+    @InjectMetric('user_find_all_total')
+    private readonly userFindAllCounter: Counter,
+    @InjectMetric('user_find_one_total')
+    private readonly userFindOneCounter: Counter,
+    @InjectMetric('user_update_total')
+    private readonly userUpdateCounter: Counter,
+    @InjectMetric('user_remove_total')
+    private readonly userRemoveCounter: Counter,
+    @InjectMetric('user_find_by_email_total')
+    private readonly userFindByEmailCounter: Counter,
     private readonly userRepository: UserRepository,
     private readonly encryptionService: EncryptionService,
   ) {}
 
   async findAll(): Promise<UserResponseDto[]> {
+    this.userFindAllCounter.inc();
     const users = await this.userRepository.find();
 
     return users.map((user) => {
@@ -26,6 +41,8 @@ export class UserService {
   }
 
   async findOne(id: number): Promise<UserResponseDto> {
+    this.userFindOneCounter.inc();
+
     const user = await this.userRepository.findOne({ where: { id } });
 
     if (!user) {
@@ -49,20 +66,28 @@ export class UserService {
 
     const savedUser = await this.userRepository.save(user);
 
+    this.userCreatedCounter.inc();
+
     const { id, username, email } = savedUser;
     return { id, username, email };
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    this.userUpdateCounter.inc();
+
     const { username, email } = updateUserDto;
     return this.userRepository.updateUser(id, username, email);
   }
 
   async remove(id: number): Promise<void> {
+    this.userRemoveCounter.inc();
+
     await this.userRepository.deleteUser(id);
   }
 
   async findByEmail(email: string): Promise<User | undefined> {
+    this.userFindByEmailCounter.inc();
+
     return this.userRepository.findByEmail(email);
   }
 }
