47cfb012813da9898a09370efa41445e02567a1a
Romulo Freires <romulofreires1@gmail.com>
2024-09-04 15:52:34 -0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
   await app.listen(3000);
   console.log('Application is running on: http://localhost:3000/api');
 }
